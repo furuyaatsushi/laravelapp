87df0b6c0ffc144b86296214a3d0b8b937a49eb6
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-25 00:23:48 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('hello/{id?}/{pass?}', 'HelloController@index');
+Route::get('hello', 'HelloController@index');
+Route::get('hello/other', 'HelloController@Other');
