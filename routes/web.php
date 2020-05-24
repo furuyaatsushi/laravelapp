aed19215879c6a34eeebd88d8294bcbd2bea2b45
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-25 00:05:41 +0900

    action practice

--- routes/web.php
+++ routes/web.php
@@ -14,22 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('hello/{msg?}', function($msg='no message.'){
-$html = <<<EOF
-<html>
-<head>
-<title>Hello</title>
-<style>
-body {font-size: 16px; color: #999}
-h1 {font-size: 100px; text-align: right; color: #eee; margin: -40px 0 -50px 0}
-</style>
-</head>
-<body>
-    <h1>Hello</h1>
-    <p>{$msg}</p>
-    <p>これは、サンプルで作ったページです。</p>
-</body>
-</html>
-EOF;
-    return $html;
-});
+Route::get('hello/{id?}/{pass?}', 'HelloController@index');
