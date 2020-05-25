528ddb3430da77166f3e9bd13b86425084f5af1c
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-25 23:35:32 +0900

    Create index.php

---
+++ resources/views/hello/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>Hello/Index</title>
+  <style>
+  body {font-size:16px; color:#999;}
+  h1 {font-size:30pt; text-align:right; color:#eee; margin:-15px 0 -100px 0;}
+  </style>
+</head>
+<body>
+  <h1>Index</h1>
+  <p>this is template</p>
+</body>
+</html>
