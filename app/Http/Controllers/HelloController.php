6aad14efada8b040dfda5a8ea99147f76788aa43
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-26 02:49:14 +0900

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,8 +9,9 @@ class HelloController extends Controller
 {
     public function index($id='zero'){
         $data = [
-            'msg' => 'これはコントローラから渡されたメッセージです。'
-            'id' => $id    ];
+            'msg' => 'これはコントローラから渡されたメッセージです。',
+            'id' => $id    
+        ];
         return view('hello.index', $data);
     }
 }
