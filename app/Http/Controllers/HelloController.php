0ff67f677a538e02f481c325929bcba37a901e15
furuyaatsushi <sdvxmnn@gmail.com>
2020-05-28 23:05:52 +0900

    Update HelloController.php

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -9,7 +9,16 @@ class HelloController extends Controller
 {
     public function index(){
         $data = [
-            'msg' => 'これはbladeを利用したメッセージです。'  
+            'msg' => 'お名前を入力してください。'  
+        ];
+        return view('hello.index', $data);
+    }
+
+    public function post(Request $request)
+    {
+        $msg = $request->msg;
+        $data = [
+            'msg'=>'こんにちは、' . $msg . 'さん！',
         ];
         return view('hello.index', $data);
     }
