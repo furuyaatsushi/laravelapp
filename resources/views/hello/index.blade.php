c9c0f27b12fdc075e90af60494ddd0e67dda332a
furuyaatsushi <sdvxmnn@gmail.com>
2020-06-01 23:39:23 +0900

    Update index.blade.php

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,15 +10,15 @@
   <h1>Blade/Index</h1>
   <p>&#064;forディレクティブの例</p>
   <ol>
-  @for ($i = 1;$i < 100;$i++)
-  @if ($i % 2 == 1)
-    @continue
-  @elseif ($i <= 10)
-  <li>No, {{$i}}
-  @else
-    @break
+  @foreach($data as $item)
+  @if ($loop->forst)
+  <p>データ一覧</p>
   @endif
-  @endfor
+  <li>No, {{$loop->iteration}}. {{$item}}</li>
+  @if($loop->last)
+  </ul><p>−−ここまで</p>
+  @endif
+  @endforeach
   </ol>
 </body>
 </html>
