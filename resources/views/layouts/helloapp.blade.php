dba60e9430bdab390dfcb0ef4b9b892e5e5f6dc9
furuyaatsushi <sdvxmnn@gmail.com>
2020-06-03 23:45:39 +0900

    Update helloapp.blade.php

--- resources/views/layouts/helloapp.blade.php
+++ resources/views/layouts/helloapp.blade.php
@@ -6,5 +6,17 @@
 </head>
 <body>
   <h1>@yield('title')</h1>
+  @section('menubar')
+  <h2 class="menutitle">※メニュー</h2>
+  <ul>
+    <li>@show</li>
+  </ul>
+  <hr size="1">
+  <div class="content">
+    @yield('content')
+  </div>
+  <div class="footer">
+    @yield('footer')
+  </div>
 </body>
 </html>
